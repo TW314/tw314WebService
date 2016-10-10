8dedf23a2457c7a6591a8e693c3408d01d62e427
AlanCamargo <alanhcamargo@gmail.com>
2016-10-10 01:48:38 -0300

    Criado indevidamente

--- controllers/atualizaInformacoesEmpresaController.js
+++
@@ -1,8 +0,0 @@
-module.exports = atualizaInforacoesEmpresa => {
-        const Empresa = app.db.models.empresa;
-        Perfil.update(req.body, {
-                where: {
-                    id: req.params.id
-                }
-            }
-        }
