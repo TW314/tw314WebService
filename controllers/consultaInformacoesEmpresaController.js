aae353fc5c47d35b7fcc5b25a68f978076b456a6
AlanCamargo <alanhcamargo@gmail.com>
2016-10-10 00:01:49 -0300

    DONE: Migrando para controllers

---
+++ controllers/consultaInformacoesEmpresaController.js
@@ -0,0 +1,59 @@
+module.exports = app => {
+    const Empresa = app.db.models.empresa;
+    const RamoAtividade = app.db.models.ramo_atividade;
+};
+
+exports.list = callback => {
+
+
+    Empresa.findAll({
+            where: {},
+            attributes: {
+                exclude: ['ramoAtividadeId']
+            },
+            include: [{
+                model: RamoAtividade,
+                attributes: ['id', 'nome']
+            }]
+        })
+        .then(result => {
+            if (result) {
+                callback.json(result);
+            } else {
+                callback.sendStatus(404);
+            }
+        })
+        .catch(error => {
+            callback.status(412).json({
+                msg: error.message
+            });
+        });
+};
+
+module.exports.empresa = (id, callback) => {
+    const Empresa = app.db.models.empresa;
+    const RamoAtividade = app.db.models.ramo_atividade;
+
+    Empresa.findAll({
+            where: {},
+            attributes: {
+                exclude: ['ramoAtividadeId']
+            },
+            include: [{
+                model: RamoAtividade,
+                attributes: ['id', 'nome']
+            }]
+        })
+        .then(result => {
+            if (result) {
+                res.json(result);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(error => {
+            res.status(412).json({
+                msg: error.message
+            });
+        });
+};
