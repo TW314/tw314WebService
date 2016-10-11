e5787ff5b8b2d1a1d7ff787102947c083423cfcb
AlanCamargo <alanhcamargo@gmail.com>
2016-10-11 02:13:15 -0300

    Migrado logica para controller

--- routes/consultaInformacoesUsuariosPorPerfil.js
+++ routes/consultaInformacoesUsuariosPorPerfil.js
@@ -1,34 +1,11 @@
 module.exports = app => {
-    const Usuario = app.db.models.usuario;
-    const Empresa = app.db.models.empresa;
-    const Perfil = app.db.models.perfil;
+
+    const controllers = app.controllers.consultaInformacoesUsuariosPorPerfilController
+
     app.route("/consultaUsuariosPorPerfil/:id")
         .get((req, res) => {
-            Perfil.find({
-                    where: {
-                        id: req.params.id
-                    },
-                    attributes: ['nome'],
-                    include: [{
-                        model: Usuario,
-                        attributes: ['id', 'nome', 'email', 'status_ativacao'],
-                        include: [{
-                            model: Empresa,
-                            attributes: ['id', 'razao_social', 'status_ativacao']
-                        }]
-                    }]
-                })
-                .then(result => {
-                    if (result) {
-                        res.json(result);
-                    } else {
-                        res.sendStatus(404);
-                    }
-                })
-                .catch(error => {
-                    res.status(412).json({
-                        msg: error.message
-                    });
-                });
+            controllers.list(app, req.params.id, resp => {
+                res.json(resp)
+            });
         });
 };
