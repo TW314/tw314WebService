283fa7fc31275f36df0cc6653fe90fb50e41b4e4
Alan Camargo <alanhcamargo@gmail.com>
2016-10-12 19:01:34 -0300

    Arrumado o erro de sintaxe

--- routes/cadastraEmpresa.js
+++ routes/cadastraEmpresa.js
@@ -1,11 +1,11 @@
 module.exports = app => {
 
-    const controllers = app.controllers.consultaInformacoesEmpresaController;
+    const controllers = app.controllers.cadastraEmpresaController;
 
     app.route("/cadastraEmpresa") //middelware de pre-execucao das rotas
         .post((req, res) => {
-                controllers.cadastraEmpresa(req.body, req.params, app , resp => {
-                    res.json(resp)
-                }););
+            controllers.cadastraEmpresa(req.body, req.params, app, resp => {
+                res.json(resp)
+            });
         });
 };
