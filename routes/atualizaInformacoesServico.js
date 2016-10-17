d97183c8cc8f2e73d549ff99f2ea93b6a8e3102f
AlanCamargo <alanhcamargo@gmail.com>
2016-10-17 15:50:38 -0200

    Feito controller servico

--- routes/atualizaInformacoesServico.js
+++ routes/atualizaInformacoesServico.js
@@ -1,16 +1,11 @@
 module.exports = app => {
-  const Servico = app.db.models.servico;
 
-  app.route("/atualizaInforacoesServico/:id")
-    .put((req, res) => {
-      Perfil.update(req.body, {
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(result => res.sendStatus(204))
-      .catch(error => {
-        res.status(412).json({msg: error.message});
-      });
-    })
+    const controllers = app.controllers.atualizaInformacoesServicoController;
+
+    app.route("/atualizaInformacoesServico/:id")
+        .put((req, res) => {
+            controllers.atualizaInformacoesServico(req.body, req.params, app, resp => {
+                res.json(resp)
+            });
+        })
 };
