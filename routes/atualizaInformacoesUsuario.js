61ecff63a77fe33d4ed11b7b16a7521c450c195e
AlanCamargo <alanhcamargo@gmail.com>
2016-10-17 15:50:57 -0200

    Feito controller usuario

--- routes/atualizaInformacoesUsuario.js
+++ routes/atualizaInformacoesUsuario.js
@@ -1,16 +1,11 @@
 module.exports = app => {
-  const Usuario = app.db.models.usuario;
 
-  app.route("/atualizaInforacoesUsuario/:id")
-    .put((req, res) => {
-      Perfil.update(req.body, {
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(result => res.sendStatus(204))
-      .catch(error => {
-        res.status(412).json({msg: error.message});
-      });
-    })
+    const controllers = app.controllers.atualizaInformacoesUsuarioController;
+
+    app.route("/atualizaInformacoesUsuario/:id")
+        .put((req, res) => {
+            controllers.atualizaInformacoesUsuario(req.body, req.params, app, resp => {
+                res.json(resp)
+            });
+        })
 };
