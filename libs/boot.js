9a27cac43f1bb3bb63dcc307bdfadc0bbc912e33
AlanCamargo <alanhcamargo@gmail.com>
2016-10-23 21:28:06 -0200

    importado debug para boot

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,9 @@
+import debug from 'debug'
+
 module.exports = app => {
     app.db.sequelize.sync().done(() => {
-        app.listen(app.get("port"), () => { //app.listen > funcao responsavel por iniciar o servidor 
-            console.log('NTask API - porta ${app.get("port")}');
+        app.listen(app.get("port"), () => { //app.listen > funcao responsavel por iniciar o servidor
+            debug(`TW314 API - porta ${app.get("port")}`);
         });
     });
 }
