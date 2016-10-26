7def3293a684ed117b5cb61bbcb107548f310c8c
AlanCamargo <alanhcamargo@gmail.com>
2016-10-26 16:32:29 -0200

    .

--- controllers/ticket/ticketController.js
+++ controllers/ticket/ticketController.js
@@ -1,41 +1,41 @@
 module.exports.obterTicketPorCodigoDeAcesso = (app, id, callback) => {
 
-const Ticket = app.db.models.ticket;
-const RelacionamentoEmpresaServico = app.db.models.relacionamento_emp_svc;
-const Empresa = app.db.models.empresa;
-const Servico = app.db.models.servico;
-const StatusTicket = app.db.models.status_ticket
+    const Ticket = app.db.models.ticket;
+    const RelacionamentoEmpresaServico = app.db.models.relacionamento_emp_svc;
+    const Empresa = app.db.models.empresa;
+    const Servico = app.db.models.servico;
+    const StatusTicket = app.db.models.status_ticket
 
-Ticket.findOne({
-        where: {
-            codigo_acesso: id
-        },
-        attributes: ['numero_ticket'],
-        include: [{
-            model: StatusTicket,
-            attributes: ['id', 'nome']
-        }, {
-            model: RelacionamentoEmpresaServico,
-            attributes: ['status_ativacao'],
+    Ticket.findOne({
+            where: {
+                codigo_acesso: id
+            },
+            attributes: ['numero_ticket'],
             include: [{
-                model: Servico,
-                attributes: ['id', 'nome', 'sigla']
+                model: StatusTicket,
+                attributes: ['id', 'nome']
             }, {
-                model: Empresa,
-                attributes: ['id', 'razao_social']
-            }, ]
-        }]
-    })
-    .then(result => {
-        if (result) {
-            callback(result);
-        } else {
-            callback(404);
-        }
-    })
-    .catch(error => {
-        callback({
-            error: error.message
+                model: RelacionamentoEmpresaServico,
+                attributes: ['status_ativacao'],
+                include: [{
+                    model: Servico,
+                    attributes: ['id', 'nome', 'sigla']
+                }, {
+                    model: Empresa,
+                    attributes: ['id', 'razao_social']
+                }]
+            }]
         })
-    })
+        .then(result => {
+            if (result) {
+                callback(result);
+            } else {
+                callback(404);
+            }
+        })
+        .catch(error => {
+            callback({
+                error: error.message
+            })
+        })
 }
