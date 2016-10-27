06b54776d712e910374adb4c0ff125b110cfc5c2
AlanCamargo <alanhcamargo@gmail.com>
2016-10-27 20:31:47 -0200

    Feito relatorio

--- controllers/relatorio/relatorioController.js
+++ controllers/relatorio/relatorioController.js
@@ -1,18 +1,37 @@
-module.exports.obterRelatorio = (app, id, nome, callback) => {
+module.exports.obterRelatorio = (app, nome, dataInicio, dataFim, callback) => {
 
     const Usuario = app.db.models.usuario;
     const Atendimento = app.db.models.atendimento;
     const Servico = app.db.models.servico;
     const Ticket = app.db.models.ticket;
     const RelacionamentoEmpresaServico = app.db.models.relacionamento_emp_svc;
+    const StatusAtendimento = app.db.models.status_atendimento;
 
-    Atendimento.findAll({
-            where: {},
+    const data_Inicio = new Date(dataInicio);
+    const data_Fim = new Date(dataFim);
+
+    Atendimento.find({
+            where: {
+                $and: [{
+                    data_hora_inicio: {
+                      $between: [data_Inicio, data_Fim]
+                    }
+                }, {
+                    data_hora_fim: {
+                      $between: [data_Inicio, data_Fim]
+                    }
+                }]
+            },
+            attributes: {
+                exclude: ['usuarioId', 'ticketCodigoAcesso', 'statusAtendimentoId']
+            },
             include: [{
+                model: StatusAtendimento
+            }, {
                 model: Usuario,
                 attributes: ['id', 'nome', 'status_ativacao'],
                 where: {
-                    //nome: nome
+                    nome: nome
                 }
             }, {
                 model: Ticket,
@@ -21,7 +40,7 @@ module.exports.obterRelatorio = (app, id, nome, callback) => {
                     model: RelacionamentoEmpresaServico,
                     attributes: ['status_ativacao'],
                     include: [{
-                      model: Servico
+                        model: Servico
                     }]
                 }]
             }]
--- routes/relatorio/relatorioRouter.js
+++ routes/relatorio/relatorioRouter.js
@@ -2,9 +2,9 @@ module.exports = app => {
 
     const controllers = app.controllers.relatorio.relatorioController;
 
-    app.route("/relatorio/")
+    app.route("/relatorio/:nome&:dataInicio&:dataFim")
         .get((req, res) => {
-            controllers.obterRelatorio(app, req.params.id, req.params.nome, resp => {
+            controllers.obterRelatorio(app, req.params.nome, req.params.dataInicio, req.params.dataFim, resp => {
                 res.json(resp)
             })
         })
