6fa92fc83b53a9c8803ce8504e96ecf8068a81fa
AlanCamargo <alanhcamargo@gmail.com>
2016-11-06 15:43:44 -0200

    Feito pesquisa de empresa por cnpj

--- controllers/empresa/empresaController.js
+++ controllers/empresa/empresaController.js
@@ -33,17 +33,48 @@ module.exports.obterEmpresaPorId = (app, id, callback) => {
     const RamoAtividade = app.db.models.ramo_atividade;
 
     Empresa.findOne({
-            where: {
-                id: id
-            },
-            attributes: {
-                exclude: ['ramoAtividadeId']
-            },
-            include: [{
-                model: RamoAtividade,
-                attributes: ['id', 'nome']
-            }]
+        where: {
+            id: id
+        },
+        attributes: {
+            exclude: ['ramoAtividadeId']
+        },
+        include: [{
+            model: RamoAtividade,
+            attributes: ['id', 'nome']
+        }]
+    })
+        .then(result => {
+            if (result) {
+                callback(result);
+            } else {
+                callback(404);
+            }
         })
+        .catch(error => {
+            callback({
+                error: error.message
+            });
+        });
+};
+
+module.exports.obterEmpresaPorCNPJ = (app, cnpj, callback) => {
+
+    const Empresa = app.db.models.empresa;
+    const RamoAtividade = app.db.models.ramo_atividade;
+
+    Empresa.findOne({
+        where: {
+            numero_cnpj: cnpj
+        },
+        attributes: {
+            exclude: ['ramoAtividadeId']
+        },
+        include: [{
+            model: RamoAtividade,
+            attributes: ['id', 'nome']
+        }]
+    })
         .then(result => {
             if (result) {
                 callback(result);
