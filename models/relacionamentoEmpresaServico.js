8ce7721bd94d37fa01fdfa40f76430f62906300c
HaluDeCassia <haluanedecassia@gmail.com>
2016-11-22 17:44:45 -0200

    [FIXED] retirando campo de status do relacionamento empresa serviço

--- models/relacionamentoEmpresaServico.js
+++ models/relacionamentoEmpresaServico.js
@@ -1,12 +1,6 @@
 module.exports = (sequelize, DataType) => {
     const RelacionamentoEmpresaServico = sequelize.define("relacionamento_emp_svc", {
-        status_ativacao: {
-            type: DataType.ENUM('Ativo', 'Inativo'),
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        }
+
     }, {
         freezeTableName: true,
         classMethods: {
