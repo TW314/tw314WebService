f26eb199dd48f0cce6aabede0e83ab56e08f3e1c
Alan Camargo <alanhcamargo@gmail.com>
2016-11-24 21:53:44 -0200

    tirado autenticação get ticket

--- libs/boot.js
+++ libs/boot.js
@@ -1,9 +1,9 @@
-import debug from 'debug'
+import socket from 'socket.io'
 
 module.exports = app => {
     app.db.sequelize.sync().done(() => {
-        app.listen(app.get("port"), () => { //app.listen > funcao responsavel por iniciar o servidor
+        socket.listen(app.listen(app.get("port"), () => { //app.listen > funcao responsavel por iniciar o servidor
             console.log(`TW314 API - porta ${app.get("port")}`);
-        });
+        }));
     });
-}
+};
--- routes/ticket/ticketRouter.js
+++ routes/ticket/ticketRouter.js
@@ -5,7 +5,7 @@ module.exports = app => {
     const controllers = app.controllers.ticket.ticketController;
 
     app.route("/ticket/:id")
-        .get(stormpath.apiAuthenticationRequired, (req, res) => {
+        .get((req, res) => {
             controllers.obterTicketPorCodigoDeAcesso(app, req.params.id, resp => {
                 res.json(resp)
             })
