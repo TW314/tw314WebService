cb9380c063e68295599768d1defc098d10809bc3
HaluDeCassia <haluanedecassia@gmail.com>
2016-11-25 02:35:15 -0200

    [FIXED] arrumando parametros de consulta de obter ticket por ordem sequencial

--- controllers/ticket/ticketController.js
+++ controllers/ticket/ticketController.js
@@ -92,32 +92,7 @@ module.exports.atualizaStatusTicket = (body, id, app, callback) => {
 
 module.exports.obterTicketOrdemSequencial = (app, statusTicketId, empresaId, servicoId, data_hora_emissao, callback) => {
 
-    const query = "select numero_ticket, " +
-        "servico.sigla as servico_sigla, " +
-        "servico.id as servico_id, " +
-        "servico.nome as servico_nome, " +
-        "status_ticket.id as status_id, " +
-        "status_ticket.nome as status_nome, " +
-        "empresa.id as empresa_id, " +
-        "empresa.nome_fantasia as empresa_nome, " +
-        "prioridade_ticket.id as prioridade_id, " +
-        "prioridade_ticket.nome as prioridade_nome " +
-        "from ticket " +
-        "left outer join status_ticket " +
-        " on statusTicketId = status_ticket.id " +
-        "left outer join relacionamento_emp_svc " +
-        " on ticket.servicoId = relacionamento_emp_svc.servicoId and ticket.empresaId = relacionamento_emp_svc.empresaId " +
-        "left outer join servico " +
-        "on servicoId = servico.id " +
-        "left outer join empresa " +
-        "on empresaId = empresa.id " +
-        "left outer join prioridade_ticket " +
-        "on prioridadeTicketId = prioridade_ticket.id " +
-        "where date(data_hora_emissao) = :data_hora_emissao " +
-        "and ticket.empresaId = :empresaId " +
-        "and ticket.servicoId = :servicoId " +
-        "and statusTicketId = :statusTicketId " +
-        "order by numero_sequencial ASC limit 6;";
+    const query = "select concat(`relacionamento_emp_svc.servico`.sigla, numero_ticket) as ticket, `relacionamento_emp_svc.servico`.nome as servico_nome, status_ticket.id as status_id,   status_ticket.nome as status_nome,     `relacionamento_emp_svc.empresa`.id as empresa_id,    `relacionamento_emp_svc.empresa`.nome_fantasia as empresa_nome,    prioridade_ticket.id as prioridade_id,    prioridade_ticket.nome as prioridade_nome from ticket	left outer join status_ticket	 on statusTicketId = status_ticket.id	left outer join prioridade_ticket	 on prioridadeTicketId = prioridade_ticket.id	left outer join relacionamento_emp_svc     on ticket.servicoId = relacionamento_emp_svc.servicoId and ticket.empresaId = relacionamento_emp_svc.empresaId	left outer join servico AS `relacionamento_emp_svc.servico`     on relacionamento_emp_svc.servicoId = `relacionamento_emp_svc.servico`.id left outer join empresa as `relacionamento_emp_svc.empresa`     on relacionamento_emp_svc.empresaId = `relacionamento_emp_svc.empresa`.id where date(data_hora_emissao) = date(:data_hora_emissao)  and ticket.empresaId = :empresaId and ticket.servicoId = :servicoId and statusTicketId = :statusTicketId  order by numero_sequencial ASC limit 6;";
 
     app.db.sequelize.query(query, {
         replacements: {
